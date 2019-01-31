2c87643a288fa6c57512e3796b414658582498c4
Tom Holmes <tholmes59@gmail.com>
2019-01-31 00:35:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,13 +14,13 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
-  # def self.all
-  #   @@songs
-  # end
+  def self.all
+    @@songs
+  end
 
   def artist=(artist)
     @artist = artist
